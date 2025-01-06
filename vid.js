99bd3ac333eb07dc4054544585af1bdd3d8b2be8
nusht <webheysnu@gmail.com>
2025-01-06 09:58:35 +0530

    update

--- vid.js
+++ vid.js
@@ -1,8 +1,1 @@
-var video = document.querySelector('my-video');
-video.controls; // true
-video.controlsList; // "nofullscreen nodownload noremoteplayback noplaybackrate" - "foobar" not present
-video.controlsList.remove('noremoteplayback');
-video.controlsList; // "nofullscreen nodownload noplaybackrate" - "noremoteplayback" not present
-video.getAttribute('controlslist'); //
-
-document.addEventListener('contextmenu', event => event.preventDefault());
+$(document).ready(function() { $("my-video").on("contextmenu", function(e ){ return false; }); });
